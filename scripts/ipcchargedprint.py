6d9c3ab6a6b692c3c8b80a9769ad00bf760d7d23
Shamanth M Hiremath <123861244+ShamanthHiremath@users.noreply.github.com>
2024-09-03 01:15:50 +0530

    scripts added

---
+++ scripts/ipcchargedprint.py
@@ -0,0 +1,7 @@
+# IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+#from server.scripts.ipc.ipccharged.ipccharged import *
+#path_vector_store = "Bail_Saarathi/vector_database/IPC_Sections_VectorEmbeddings/"
+#bail_applicant_case_record_text = input("Case RECORD: ")
+
+#crime_to_ipc = IPC_Sections_Charged(bail_applicant_case_record_text, path_vector_store)
+#print(crime_to_ipc)
